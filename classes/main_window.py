772a549546f0f08cd45217ceff9fd020764aa9eb
irumvaa <irumvaa@berea.edu>
2026-04-21 23:17:31 -0400

    Finalizing creating BaseCipher class for all ciphers

--- classes/main_window.py
+++ classes/main_window.py
@@ -1,6 +1,6 @@
 ######################################################################
 # Author: Alain Irumva and Jayden Fleming
-# Username: irumvaa and jfleming
+# Username: irumvaa and flemingj
 #
 # Assignment: Final project (HW10 Expansion): Base Cipher System
 #
@@ -27,3 +27,32 @@ class BaseCipher:
         """
         self.name = name
         self.key = key
+
+    def encode(self, text):
+        """
+        Encrypts the given input text using the cipher's rules.
+
+        :param text: plaintext input
+        :return: encrypted text
+        """
+        raise NotImplementedError("encode() must be implemented by subclass") #Forces child classes to implement their own encode logic
+
+    def decode(self, text):
+        """
+        Decrypts the given input text using the cipher's rules.
+        This method should be overridden by subclasses.
+
+        :param text: encrypted input
+        :return: decrypted text
+        """
+        raise NotImplementedError("decode() must be implemented by subclass")
+
+    def validate_input(self, text):
+        """
+        Checks if the input text is valid for the cipher.
+        Default behavior ensures the input is a string.
+
+        :param text: input text
+        :return: True if valid, False otherwise
+        """
+        return isinstance(text, str)
