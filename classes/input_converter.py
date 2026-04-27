936759c660007da895e948e4d4feb04f8991a4dd
irumvaa <irumvaa@berea.edu>
2026-04-27 15:56:31 -0400

    Finished creating input_converter.py class chiphers can process

--- classes/input_converter.py
+++ classes/input_converter.py
@@ -4,3 +4,22 @@ class InputConverter:
     Handles converting user input into a format
     usable by cipher classes.
     """
+    def __int__(self, source=None):
+        """Initializes the input source.
+        Source can be a file path or raw text """
+        self.source = source #stores the input source inside the object
+
+    def from_file(self, file_path):
+        """ Reads text input from a file"""
+        with open(file_path, "r", encoding="utf-8") as file:
+            return file.read()
+
+    def from_text(self, text):
+        """ Accepts direct text input (GUI or CLI)"""
+        if not isinstance((text, str)):
+            raise ValueError("Input must be a string")
+        return text
+
+    def normalize(self, text):
+        """ Normalizes text for cipher processing"""
+        return text.strip()
