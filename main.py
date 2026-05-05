05bb5712a090d216ce5c31a98784ab5ac181f543
Jayden Fleming <jfleming0726@gmail.com>
2026-05-05 18:32:24 -0400

    - implemented file exporting for text

--- main.py
+++ main.py
@@ -2,21 +2,32 @@ from classes.main_window import *
 from classes.caesar_cipher import CaesarCipher
 from classes.output_generator import OutputGenerator
 
-def encode(_event, window, config):
+def encode(_event, window, config, exporter):
     cipher = config["cipher"]
     update_key(_event, window, config)
     text = window.get_textbox_text("input")
     encoded = cipher.encode(text)
-    window.set_textbox_text("output", encoded)
+
+    if config["output"] == "text":
+        window.set_textbox_text("output", encoded)
+    elif config["output"] == "file":
+        exporter.to_file(encoded, "out_encoded.txt")
+        window.alert("Encoded text written to: ./out_encoded.txt")
+
     window.fix_focus()
     return
 
-def decode(_event, window, config):
+def decode(_event, window, config, exporter):
     cipher = config["cipher"]
     update_key(_event, window, config)
     text = window.get_textbox_text("input")
     decoded = cipher.decode(text)
-    window.set_textbox_text("output", decoded)
+
+    if config["output"] == "text":
+        window.set_textbox_text("output", decoded)
+    elif config["output"] == "file":
+        exporter.to_file(decoded, "out_decoded.txt")
+        window.alert("Decoded text written to: ./out_decoded.txt")
 
     window.fix_focus()
     return
@@ -52,9 +63,11 @@ def update_output_type(_event, window, config):
 
 
 def main():
+    # init classes
     window = MainWindow()
-    export = OutputGenerator()
+    exporter = OutputGenerator()
 
+    # mutating vars
     ciphers = {
         "caesar": CaesarCipher(0),
         "symbol": CaesarCipher(0)  # TODO
@@ -65,6 +78,7 @@ def main():
         "output": "text"
     }
 
+    # init ui elements
     window.create_gui()
 
     # ui elements to be used for bindings
@@ -73,12 +87,13 @@ def main():
     combo = window.elements["combobox"]
 
     # bind events
-    btn["encode"].bind("<Button-1>", lambda event: encode(event, window, config))
-    btn["decode"].bind("<Button-1>", lambda event: decode(event, window, config))
+    btn["encode"].bind("<Button-1>", lambda event: encode(event, window, config, exporter))
+    btn["decode"].bind("<Button-1>", lambda event: decode(event, window, config, exporter))
     entry["key"].bind("<FocusOut>", lambda event: update_key(event, window, config))
     combo["cipher"].bind("<<ComboboxSelected>>", lambda event: update_cipher(event, window, ciphers, config))
     combo["output"].bind("<<ComboboxSelected>>", lambda event: update_output_type(event, window, config))
 
+    # start event loop
     window.root.mainloop()
 
 if __name__ == "__main__":
